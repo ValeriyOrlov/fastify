8f89c17f1f1b013ee07105810e332f293bef18e8
Valerrriy Orlov <wrestlbird@gmail.com>
2024-10-01 21:58:51 +0300

    feat: pug templating engine is connected, templates are connected in user and course routes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 import fastify from 'fastify';
+import view from '@fastify/view';
+import pug from 'pug';
 
 const app = fastify({
   logger: true
 });
 const port = 3000;
 
+await app.register(view, { engine: { pug }});
+
 const state = {
   users: [
     {
       id: 1,
       name: 'user1',
+      email: 'user1@mail.com'
+    },
+    {
+      id: 2,
+      name: 'user2',
+      email: 'user2@mail.com'
     },
   ],
   courses: [
     {
       id: 1,
-      name: 'course1',
+      title: 'JS: Arrays',
+      description: 'Arrays in javascript course',
+    },
+    {
+      id: 2,
+      title: 'JS: Functions',
+      description: 'Functions in javascript course',
     },
   ],
 };
 
-app.get('/', (req, res) => {
-  res.send('Root route');
-});
+app.get('/', (req, res) => res.view('src/views/index'));
 
 app.get('/hello', (req, res) => {
   const { name } = req.query;
@@ -34,29 +48,54 @@ app.get('/hello', (req, res) => {
   }
 });
 
+app.get('/courses', (req, res) => {
+  const data = {
+    courses: state.courses,
+    header: 'Programming courses'
+  }
+
+  res.view('src/views/courses/index', data);
+});
+
 app.get('/courses/:id', (req, res) => {
   const { id } = req.params;
   const course = state.courses.find((course) => course.id === parseInt(id));
   if (!course) {
     res.code(404).send({ message: 'Course not found' });
-  } else {
-    res.send(course);
+    return;
   }
+  const data = {
+    course,
+  }
+  res.view('src/views/courses/show', data);
 });
 
 app.get('/courses/:courseId/lessons/:id', (req, res) => {
   res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
 });
 
+app.get('/users', (req, res) => {
+  const data = {
+    users: state.users,
+    header: 'Users list'
+  }
+
+  res.view('src/views/users/index', data);
+});
 
 app.get('/users/:id', (req, res) => {
   const { id } = req.params;
   const user = state.users.find((user) => user.id === parseInt(id));
   if (!user) {
     res.code(404).send({ message: 'User not found' });
-  } else {
-    res.send(user);
+    return;
   }
+
+  const data = {
+    user,
+  }
+  
+  res.view('src/views/users/show', data);
 });
 
 app.get('/users/:id/posts/:postId', (req, res) => {
