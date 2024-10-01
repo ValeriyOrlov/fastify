39494c29bf547ff198792f86428609f2c50eb57a
Valerrriy Orlov <wrestlbird@gmail.com>
2024-10-01 20:29:10 +0300

    feat: added temporary storage and get-routes of courses and users

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ const app = fastify({
 });
 const port = 3000;
 
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user1',
+    },
+  ],
+  courses: [
+    {
+      id: 1,
+      name: 'course1',
+    },
+  ],
+};
+
 app.get('/', (req, res) => {
   res.send('Root route');
 });
@@ -17,6 +32,35 @@ app.get('/hello', (req, res) => {
   } else {
     res.send('Yo!')
   }
+});
+
+app.get('/courses/:id', (req, res) => {
+  const { id } = req.params;
+  const course = state.courses.find((course) => course.id === parseInt(id));
+  if (!course) {
+    res.code(404).send({ message: 'Course not found' });
+  } else {
+    res.send(course);
+  }
+});
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
+});
+
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = state.users.find((user) => user.id === parseInt(id));
+  if (!user) {
+    res.code(404).send({ message: 'User not found' });
+  } else {
+    res.send(user);
+  }
+});
+
+app.get('/users/:id/posts/:postId', (req, res) => {
+  res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`);
 });
 
 try {
